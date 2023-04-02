3ac2d52cf95d427e29f73d9a17544096e9ad4194
Sandatang <mushi.elite@gmail.com>
2023-04-02 18:11:42 +0800

    Tkinter simplified

---
+++ Tkinter/app.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+from tkinter import ttk
+from mysql.connector import connect
+
+db = {
+    "host":"localhost",
+    "user":"root",
+    "password":"",
+    "database":"pythondb"
+}
+
+
+root = tk.Tk()
+
+root.title("Student Management")
+def login(master):
+        window = ttk.Frame(master, padding=(50, 40 , 50, 40))
+        login_frame.grid()
+        master.geometry("320x180")
+        ttk.Label(window, text="Username:").grid(column=0, row=0, sticky="E")
+        ttk.Label(window, text="Password:").grid(column=0, row=1, sticky="E")
+
+        username = tk.StringVar()
+        password = tk.StringVar()
+        ttk.Entry(window, textvariable=username).grid(column=1, row=0, sticky="W")
+        ttk.Entry(window, textvariable=password, show="*").grid(column=1, row=1,pady=5, sticky="W")
+
+
+        ttk.Button(login_frame, text="Login", command=lambda: getData(master, window, username = username.get(), password=password.get())).grid(column=1, row=2, pady=5)
+
+def getData(master, window, **kwargs):
+        if "user" in kwargs.values() and "admin" in kwargs.values():
+            window.destroy()
+
+            main_frame = ttk.Frame(master, padding = (5, 20, 5,))
+            main_frame.grid()
+            master.geometry("1215x400")
+            ttk.Label(main_frame, text="Students", font=("TkDefaultFont", 20)).grid(column=0, row=0)
+
+            treeview = ttk.Treeview(main_frame, columns=("Student Id", "Idno", "Lastname", "Firstname", "Course", "Level"), show="headings")
+
+
+            db_connectioin = connect(**db)
+            cursor = db_connectioin.cursor()
+            cursor.execute("select * from student")
+
+            for i in range(len(treeview['columns'])):
+                treeview.heading(treeview['columns'][i], text=treeview['columns'][i])
+
+            for data in reversed(cursor.fetchall()):
+               treeview.insert("", "0", values=(data[0],data[1],data[2],data[3],data[4],data[5]))
+            treeview.grid(column=0, row=1)
+
+        else:
+            ttk.Label(login_frame, text="Invalid username or password", foreground="red").grid(column=1, row=4, pady=5)
+
+
+app = login(root)
+root.mainloop()
