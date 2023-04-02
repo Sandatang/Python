047dccf0f65d43a3a0fc5662379b039a5a9db988
Sandatang <mushi.elite@gmail.com>
2023-04-02 20:01:22 +0800

    Update some constraints

--- Tkinter/app.py
+++ Tkinter/app.py
@@ -11,11 +11,10 @@ db = {
 
 
 root = tk.Tk()
-
 root.title("Student Management")
 def login(master):
         window = ttk.Frame(master, padding=(50, 40 , 50, 40))
-        login_frame.grid()
+        window.grid()
         master.geometry("320x180")
         ttk.Label(window, text="Username:").grid(column=0, row=0, sticky="E")
         ttk.Label(window, text="Password:").grid(column=0, row=1, sticky="E")
@@ -25,35 +24,47 @@ def login(master):
         ttk.Entry(window, textvariable=username).grid(column=1, row=0, sticky="W")
         ttk.Entry(window, textvariable=password, show="*").grid(column=1, row=1,pady=5, sticky="W")
 
+        ttk.Button(window, text="Login", command=lambda: validation(master, window, username = username.get(), password=password.get())).grid(column=1, row=2, pady=5)
 
-        ttk.Button(login_frame, text="Login", command=lambda: getData(master, window, username = username.get(), password=password.get())).grid(column=1, row=2, pady=5)
+def validation(master, window, **kwargs):
+    global heading
+    if "user" in kwargs.values() and "admin" in kwargs.values():
+        window.destroy()
+        main_frame = ttk.Frame(master, padding = (5, 20, 5,))
+        main_frame.grid()
+        master.geometry("1215x400")
 
-def getData(master, window, **kwargs):
-        if "user" in kwargs.values() and "admin" in kwargs.values():
-            window.destroy()
+        button = ttk.Button(main_frame, text="Show students")
+        button.grid(column=0, row=2, pady=5)
+        button.configure(command=lambda: generateDataOfStudents(main_frame, button))
+    else:
+        ttk.Label(login_frame, text="Invalid username or password", foreground="red").grid(column=1, row=4, pady=5)
 
-            main_frame = ttk.Frame(master, padding = (5, 20, 5,))
-            main_frame.grid()
-            master.geometry("1215x400")
-            ttk.Label(main_frame, text="Students", font=("TkDefaultFont", 20)).grid(column=0, row=0)
+def generateDataOfStudents(window, button):
+    button.grid_remove()
+    treeview = ttk.Treeview(window, columns=("Student Id", "Idno", "Lastname", "Firstname", "Course", "Level"), show="headings")
 
-            treeview = ttk.Treeview(main_frame, columns=("Student Id", "Idno", "Lastname", "Firstname", "Course", "Level"), show="headings")
+    db_connection = connect(**db)
+    cursor = db_connection.cursor()
+    cursor.execute("select * from student")
 
+    for i in range(len(treeview['columns'])):
+        treeview.heading(treeview['columns'][i], text=treeview['columns'][i])
+    for data in reversed(cursor.fetchall()):
+       treeview.insert("", "0", values=(data[0],data[1],data[2],data[3],data[4],data[5]))
+    treeview.grid(column=0, row=1)
+    
+    hide = ttk.Button(window, text="Hide")
+    hide.grid(column=0, row=2, pady=50)
+    hide.configure(command=lambda: clearTreeview(treeview, hide=hide, show=button))
 
-            db_connectioin = connect(**db)
-            cursor = db_connectioin.cursor()
-            cursor.execute("select * from student")
+    cursor.close()
+    db_connection.close()
 
-            for i in range(len(treeview['columns'])):
-                treeview.heading(treeview['columns'][i], text=treeview['columns'][i])
-
-            for data in reversed(cursor.fetchall()):
-               treeview.insert("", "0", values=(data[0],data[1],data[2],data[3],data[4],data[5]))
-            treeview.grid(column=0, row=1)
-
-        else:
-            ttk.Label(login_frame, text="Invalid username or password", foreground="red").grid(column=1, row=4, pady=5)
-
+def clearTreeview(treeview, **button):
+    treeview.grid_remove()
+    button['show'].grid()
+    button['hide'].grid_remove()
 
 app = login(root)
 root.mainloop()
